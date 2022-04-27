f0c061d7cdb577232f49b06ca818af36f9c014f5
Kiryl Yermalovich <ts.underawater@gmail.com>
2022-04-27 18:58:46 +0300

    add: eslint and root styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import { AppProps } from 'next/dist/shared/lib/router/router';
+import Head from 'next/head';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return <>
+    <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet" />
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
 
-export default MyApp
+export default MyApp;
