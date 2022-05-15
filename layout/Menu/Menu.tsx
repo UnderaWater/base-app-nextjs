87523121ea9f0a47e73cb984363664f1fa8fa45c
Kiryl Yermalovich <ts.underawater@gmail.com>
2022-05-15 19:34:54 +0300

    fix: fixed menu

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -22,8 +22,6 @@ const Menu: React.FC = () => {
     const { menu, setMenu, firstCategory } = useContext(AppContext);
     const router = useRouter();
 
-    console.log(menu)
-
     const openSecondLevel = (secondCategory: string) => {
         setMenu && setMenu(menu.map(m => {
             if (m._id.secondCategory === secondCategory) {
@@ -40,14 +38,16 @@ const Menu: React.FC = () => {
                 {firstLevelMenu.map(m => (
                     <div key={m.route}>
                         <Link href={`/${m.route}`}>
-                            <p className={cn(styles.firstLevel, {
-                                [styles.firstLevelActive]: m.id === firstCategory
-                            })}>
-                                {m.icon}
-                                <span>{m.name}</span>
-                            </p>
+                            <a>
+                                <div className={cn(styles.firstLevel, {
+                                    [styles.firstLevelActive]: m.id == firstCategory
+                                })}>
+                                    {m.icon}
+                                    <span >{m.name}</span>
+                                </div>
+                            </a>
                         </Link>
-                        {m.id === firstCategory && buildSecondLevel(m)}
+                        {m.id == firstCategory && buildSecondLevel(m)}
                     </div>
                 ))}
             </>
@@ -55,6 +55,7 @@ const Menu: React.FC = () => {
     };
 
     const buildSecondLevel = (menuItem: IFirstLevelMenuItem) => {
+        console.log('menu item from second: ', menuItem);
         return (
             <div className={styles.secondBlock}>
                 {menu.map(m => {
@@ -63,9 +64,7 @@ const Menu: React.FC = () => {
                     }
                     return (
                         <div key={m._id.secondCategory}>
-                            <div className={styles.secondLevel} onClick={() => openSecondLevel(m._id.secondCategory)}>
-                                {m._id.secondCategory}
-                            </div>
+                            <div className={styles.secondLevel} onClick={() => openSecondLevel(m._id.secondCategory)}>{m._id.secondCategory}</div>
                             <div className={cn(styles.secondLevelBlock, {
                                 [styles.secondLevelBlockOpened]: m.isOpened
                             })}>
@@ -83,7 +82,7 @@ const Menu: React.FC = () => {
             pages.map(p => (
                 <Link href={`/${route}/${p.alias}`}>
                     <a className={cn(styles.thirdLevel, {
-                        [styles.thirdLevelActive]: `/${route}/${p.alias}` == router.asPath
+                        [styles.thirdLevelActive]: `/${route}/${p.alias}` === router.asPath
                     })}>
                         {p.category}
                     </a>
