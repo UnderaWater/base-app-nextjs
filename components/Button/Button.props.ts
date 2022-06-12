c7756e3f79c324529c2db05e88c34555142dbf19
Kiryl Yermalovich <ts.underawater@gmail.com>
2022-06-12 17:24:37 +0300

    add: added animation for button components

--- components/Button/Button.props.ts
+++ components/Button/Button.props.ts
@@ -1,6 +1,8 @@
 import { ButtonHTMLAttributes, DetailedHTMLProps, ReactNode } from "react";
 
-export interface IButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+export interface IButtonProps extends
+    Omit<DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>,
+    'onAnimationStart' | 'onDragStart' | 'onDragEnd' | 'onDrag' | 'ref'> {
     children: ReactNode;
     appearance: 'primary' | 'ghost';
     arrow?: 'right' | 'down' | 'none';
--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -3,23 +3,24 @@ import { IButtonProps } from './Button.props';
 import cn from 'classnames';
 import styles from './Button.module.css';
 import ArrowSvg from '../ArrowSvg/ArrowSvg';
+import { motion } from 'framer-motion';
 
 const Button: React.FC<IButtonProps> = ({ appearance, children, className, arrow = 'none', ...props }) => {
     return (
-        <button className={cn(styles.button, className, {
+        <motion.button className={cn(styles.button, className, {
             [styles.primary]: appearance === 'primary',
             [styles.ghost]: appearance === 'ghost',
         })}
             {...props}
+            whileHover={{ scale: 1.05 }}
         >
             {children}
             {arrow !== 'none' && <span className={cn(styles.arrow, {
                 [styles.down]: arrow === 'down',
-
             })}>
                 <ArrowSvg />
             </span>}
-        </button>
+        </motion.button>
     );
 };
 
